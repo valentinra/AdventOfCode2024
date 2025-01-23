10190a466aae813a08c969fb19b9b147a38f4442
Valentin Raceanu <valentin.raceanu@gmail.com>
2025-01-23 09:44:08 +0200

    Add Day8 template files

---
+++ AdventOfCode.Tests/Day8Tests.cs
@@ -0,0 +1,58 @@
+﻿using AdventOfCode.day08;
+using Xunit.Abstractions;
+
+namespace AdventOfCode.Tests
+{
+    public class Day8Tests
+    {
+        private readonly ITestOutputHelper _output;
+
+        public Day8Tests(ITestOutputHelper output)
+        {
+            _output = output;
+            // Redirect console output to the test output Console.SetOut(new ConsoleOutputHelper(output));
+            Console.SetOut(new ConsoleOutputHelper(output));
+        }
+
+        readonly string input = """
+                ............
+                ........0...
+                .....0......
+                .......0....
+                ....0.......
+                ......A.....
+                ............
+                ............
+                ........A...
+                .........A..
+                ............
+                ............
+                """;
+
+        [Fact]
+        public void TestPart1()
+        {
+            // Arrange
+            var Day8 = new Day8();
+
+            // Act
+            var result = Day8.GetPart1Result(input);
+
+            // Assert
+            Assert.Equal(14, result);
+        }
+
+        [Fact]
+        public void TestPart2()
+        {
+            // Arrange
+            var Day8 = new Day8();
+
+            // Act
+            Day8.GetPart2Result(input);
+
+            // Assert
+            //Assert.Equal(11387, result);
+        }
+    }
+}
---
+++ AdventOfCode/day08/Day8.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.day08
+{
+    public class Day8
+    {
+        public int GetPart1Result(string? input = null)
+        {
+            int result = 0;
+
+            ParseInput(input ?? File.ReadAllText("..\\..\\..\\day08\\input.txt"));
+
+            return result;
+        }
+
+        public void GetPart2Result(string? input = null)
+        {
+
+        }
+
+        public void ParseInput(string input)
+        {
+            Console.WriteLine("Parsing input");
+        }
+    }
+}
